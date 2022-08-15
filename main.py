557edb90a0c92d9bbcc4e94824986825562c5847
Max Gonzih <gonzih@gmail.com>
2022-08-15 14:39:16 -0700

    Render shit

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import time
 
 slider = rt.slider(default = 5)
 rt.md('=' * slider.value)
-rt.md(f"Current value is {slider.value}")
-rt.md(f"I'm only visible when {slider.value} > 1", visible = slider.value > 1)
+rt.md(f"### Current value is {slider.value}")
+rt.md(f"I'm only **visible** when {slider.value} > 1", visible = slider.value > 1)
 rt.md(f"I'm only visible when {slider.value} < 99", visible = slider.value < 99)
 rt.button("Click me", on_click=lambda: print("\n\n\nClicked\n\n\n"))
 
