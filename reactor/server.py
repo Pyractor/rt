ff5fce78831773e314f01013a51f5988bba0e787
Max Gonzih <gonzih@gmail.com>
2022-08-15 11:02:24 -0700

    Ugh

--- reactor/server.py
+++ reactor/server.py
@@ -1,10 +1,34 @@
 import asyncio
 import websockets
+import json
+from typing import List, Optional, Union, Any
+from pydantic import BaseModel
+
+class Hello(BaseModel):
+    session: str
+
+class Change(BaseModel):
+    id: str
+    value: Any
+
+class StateSync(BaseModel):
+    state: Any
+
+class MessageIn(BaseModel):
+    kind: str
+    message: Union[Hello, Change]
+
+class MessageOut(BaseModel):
+    kind: str
+    message: Union[StateSync, Hello]
 
 async def echo(websocket):
     async for message in websocket:
+        msg = MessageIn.parse_raw(message)
         print(message)
-        await websocket.send(message)
+        print(msg)
+        if msg.kind == "Hello":
+            await websocket.send(StateSync(state="bebebe").json())
 
 async def main():
     host = "localhost"
