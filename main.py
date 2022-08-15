75fadfa986b472f57ad88f71a8460b073922440d
Max Gonzih <gonzih@gmail.com>
2022-08-15 14:35:24 -0700

    Oh ok

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import reactor.runtime as rt
 import time
 
 slider = rt.slider(default = 5)
+rt.md('=' * slider.value)
 rt.md(f"Current value is {slider.value}")
 rt.md(f"I'm only visible when {slider.value} > 1", visible = slider.value > 1)
 rt.md(f"I'm only visible when {slider.value} < 99", visible = slider.value < 99)
