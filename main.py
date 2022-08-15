a6b4f15683574ea7bed8832a825b9c873080083c
Max Gonzih <gonzih@gmail.com>
2022-08-15 12:51:22 -0700

    Ugh

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ slider = rt.slider(default = 5)
 rt.md(f"Current value is {slider.value}")
 rt.md(f"I'm only visible when {slider.value} > 1", visible = slider.value > 1)
 rt.md(f"I'm only visible when {slider.value} < 99", visible = slider.value < 99)
+rt.button("Click me", on_click=lambda: print("\n\n\nClicked\n\n\n"))
 
 print(rt.__REGISTRY)
--- reactor/runtime.py
+++ reactor/runtime.py
@@ -1,6 +1,6 @@
 import traceback
 import hashlib
-from typing import List, Optional, Dict, Any
+from typing import List, Optional, Dict, Any, Callable
 from pydantic import BaseModel
 
 class Slider(BaseModel):
@@ -17,10 +17,21 @@ class Markdown(BaseModel):
     visible: bool = True
     md: str = ""
 
+class Button(BaseModel):
+    id: str
+    kind: str = "Button"
+    visible: bool = True
+    label: str = ""
+
+global __CALLBACKS
 global __REGISTRY
 global __ORDER
+__CALLBACKS: Dict[str, Callable] = dict()
 __REGISTRY: Dict[str, Any] = dict()
 __ORDER: List[str] = list()
+
+def reset_callbacks():
+    __CALLBACKS = dict()
 
 def reset_registry():
     __REGISTRY = dict()
@@ -29,11 +40,14 @@ def reset_order():
     __ORDER = list()
 
 def reset():
+    reset_callbacks()
     reset_registry()
     reset_order()
 
-def register(id, obj):
+def register(id, obj, cb: Optional[Callable] = None):
     __REGISTRY[id] = obj
+    if cb != None and callable(cb):
+        __CALLBACKS[id] = cb
     if id not in __ORDER:
         __ORDER.append(id)
 
@@ -42,7 +56,11 @@ def get(id, default):
 
 def change(id: str, value: int):
     if id in __REGISTRY:
-        __REGISTRY[id].value = value
+        if hasattr(__REGISTRY[id], 'value'):
+            __REGISTRY[id].value = value
+        if id in __CALLBACKS:
+            __CALLBACKS[id]()
+
 
 def call_id():
     stack = traceback.format_stack()
@@ -50,7 +68,7 @@ def call_id():
     sha = hashlib.sha256(fmt.encode('ascii')).hexdigest()
     return sha
 
-def slider(default = 50, min = 0, max = 100, visible = True):
+def slider(default = 50, min = 0, max = 100, visible = True, on_change: Optional[Callable] = None):
     id = call_id()
     obj = get(id, Slider(id=id))
     if obj.value == None:
@@ -58,7 +76,7 @@ def slider(default = 50, min = 0, max = 100, visible = True):
     obj.min = min
     obj.max = max
     obj.visible = visible
-    register(id, obj)
+    register(id, obj, on_change)
     return obj
 
 def md(md = "", visible = True):
@@ -67,4 +85,12 @@ def md(md = "", visible = True):
     obj.md = md
     obj.visible = visible
     register(id, obj)
+    return obj
+
+def button(label = "Button",  visible = True, on_click: Optional[Callable] = None):
+    id = call_id()
+    obj = get(id, Button(id=id))
+    obj.label = label
+    obj.visible = visible
+    register(id, obj, on_click)
     return obj
